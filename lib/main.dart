283eb8373f2406d386e33293c16511a6cc643152
HARU Akebono <haru.akebono.11@gmail.com>
2018-06-14 17:31:30 +0900

    Using Material Components

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    new Center(
-      child: new Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
+}
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigator menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
       ),
-    ),
-  );
+      body: Center(
+        child: Text("hello world!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
 }
