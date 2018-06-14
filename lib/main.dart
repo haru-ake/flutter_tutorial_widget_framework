cf8c59ddec50865cae5e726ceb538949b3c20140
HARU Akebono <haru.akebono.11@gmail.com>
2018-06-14 18:07:28 +0900

    Handling gestures

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Flutter Tutorial',
-    home: TutorialHome(),
+    home: MyButton(),
   ));
 }
 
-class TutorialHome extends StatelessWidget {
+class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigator menu',
-          onPressed: null,
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
         ),
-        title: Text('Example title'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text("hello world!"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add',
-        child: Icon(Icons.add),
-        onPressed: null,
+        child: Center(
+          child: Text('Engine'),
+        ),
       ),
     );
   }
